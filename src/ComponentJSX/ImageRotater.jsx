5fe0d674d1db65f73b2bc99ff4169bbd3d72a16e
Ansh Raj Kumar <golu.kkpncc.99008@gmail.com>
2025-12-15 13:55:23 +0530

    New chnage

--- src/ComponentJSX/ImageRotater.jsx
+++ src/ComponentJSX/ImageRotater.jsx
@@ -4,22 +4,30 @@ import { Eternity2 } from "../../public/assets/assest";
 
 const AutoImageRotator = () => {
     return (
-        <section className="banner">
-            <div
-                className="slider"
-                style={{ "--quantity": Eternity2.length }}
-            >
-                {Eternity2.map((img, index) => (
-                    <div
-                        className="item"
-                        key={index}
-                        style={{ "--position": index + 1 }}
-                    >
-                        <img src={img} alt={`Slide ${index + 1}`} />
-                    </div>
-                ))}
+        <>
+
+            <div className="ImageRotHeading">
+                <h1>Image Rotater</h1>
+                <p>A 3D rotating image carousel that pauses on hover, highlights images, <br /> and smoothly brings the focused image forward.</p>
             </div>
-        </section>
+
+            <section className="banner">
+                <div
+                    className="slider"
+                    style={{ "--quantity": Eternity2.length }}
+                >
+                    {Eternity2.map((img, index) => (
+                        <div
+                            className="item"
+                            key={index}
+                            style={{ "--position": index + 1 }}
+                        >
+                            <img src={img} alt={`Slide ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
     );
 };
 
